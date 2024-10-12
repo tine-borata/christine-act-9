4967a52bff27dfa1ec07338e313750f3cb2d5ff3
JohnDenverCandia <sftdenver@gmail.com>
2024-10-12 17:02:10 +0800

    first commit

---
+++ app/Http/Controllers/ImportUsers.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB; 
+
+class ImportUsers extends Controller
+{
+    public static function import(Request $request) {
+        if(($open = fopen('C:\Users\maken\Documents\users.csv', 'r')) !== false){
+            while(($data = fgetcsv($open, 1000, ",")) !== false) {
+                DB::table("users")->insert([
+                    "fname" => $data[0],
+                    "lname" => $data[1],
+                    "age"   => $data[2]
+                ]);
+            }
+
+            return[
+                "success"  => true,
+                "massage"  => "Import successfully"
+             ];
+
+        }
+        else {     
+             return[
+                "success"  => false,
+                "massage"  => "File doesnt exist"
+             ];
+        }
+
+    }
+}
